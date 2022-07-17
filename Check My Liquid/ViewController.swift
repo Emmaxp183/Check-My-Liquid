8b04fccb724aa345744d74e4e6ed5610a1a8b5f0
Emmanuel Mensah <41510090+Emmaxp183@users.noreply.github.com>
2022-07-17 18:58:06 +0200

    Initial Commit

---
+++ Check My Liquid/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Check My Liquid
+//
+//  Created by macbook on 17/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
